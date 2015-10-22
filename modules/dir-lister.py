54e2abf0cb63fda6e4a6f065c0e6ba9810a55e3a
root <root@localhost.localdomain>
2015-10-22 17:21:57 +0200

    Adding new modules

---
+++ modules/dir-lister.py
@@ -0,0 +1,9 @@
+import os
+
+def run(**args):
+
+	print "[*] In dir-lister module."
+	files = os.listdir(".")
+
+	// list all the files of a directory
+	return str(files)
---
+++ modules/environement.py
@@ -0,0 +1,6 @@
+import os 
+
+def run(**args):
+	print"[*] In environement module."
+	return str(os.version)
+
